60257b23117a77c83f0f9f547587c76f4889ea67
peeratop02 <peeratop02@gmail.com>
2020-11-06 10:40:06 +0700

    edit attribute in index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 
 </head>
 <body>
-    <a href = "work1/index.html"> work1 </a>
-    <a href = "work2/index.html"> work2 </a>
+    <a href = "work1/index.html"> work1 </a><br>
+    <a href = "work2/index.html"> work2 </a><br>
     <a href = "work3/index.html"> work3 </a>
 
 </body>
