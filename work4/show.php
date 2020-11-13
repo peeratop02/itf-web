e7b9850d2b2356cfee6aa119a79a0e17924cda8f
peeratop02 <peeratop02@gmail.com>
2020-11-13 10:33:08 +0700

    - Edit table name in show.php

--- work4/show.php
+++ work4/show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'peeramysql.mysql.database.azure.com', 'peeratop02@peeramysql', 'MVPTop02', 'peeramysql', 3306);
+mysqli_real_connect($conn, 'peeramysql.mysql.database.azure.com', 'peeratop02@peeramysql', 'MVPTop02', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
