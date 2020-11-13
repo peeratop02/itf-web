66eeb2b630b366d158f2a8beec0b176d564d15d4
peeratop02 <peeratop02@gmail.com>
2020-11-13 10:42:07 +0700

    - Edit show.php

--- work4/show.php
+++ work4/show.php
@@ -1,6 +1,12 @@
 <html>
 <head>
 <title>ITF Lab</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -16,7 +22,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="250"> <div align="center">Link </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
