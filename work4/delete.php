987ccd360c95f80bdd037e1e745857e0033be20c
peeratop02 <peeratop02@gmail.com>
2020-11-13 12:29:14 +0700

    bruh

---
+++ work4/delete.php
@@ -0,0 +1,20 @@
+<?php 
+
+$name = $_GET['name'];
+
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'peeramysql.mysql.database.azure.com', 'peeratop02@peeramysql', 'MVPTop02', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, "DELETE FROM guestbook WHERE Name='$name'");
+
+if ($res){
+    header('Location: show.php');
+}
+else{
+    echo "Nooooooooooooo";
+}
+?>
